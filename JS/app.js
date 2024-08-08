2e6fcf64f4b976ec6bf467a0a676028829fd0f1b
Dong-Han-Kim <dh32110@gmail.com>
2024-08-09 07:38:41 +0900

    update

---
+++ JS/app.js
@@ -0,0 +1,10 @@
+let age = 32;
+let userName = 'Han';
+let hobbies = ['sports', 'cooking', 'reading'];
+let job = {
+	title: 'Developer',
+	place: 'Korea',
+};
+
+alert(hobbies[0]);
+alert(job.title);
