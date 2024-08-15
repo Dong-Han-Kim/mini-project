41246aede57a4bd0664d2e36544b80458c7baaef
Dong-Han-Kim <dh32110@gmail.com>
2024-08-16 07:39:53 +0900

    팝업

--- test/test.js
+++ test/test.js
@@ -46,3 +46,82 @@ navLinks.forEach((link) => {
 		link.classList.add('li1_active');
 	});
 });
+
+let windowWidth;
+// let index = 0;
+// let sw = 1;
+
+$(window).resize(function () {
+	windowWidth = $(window).width();
+	// 현재 슬라이드 위치 재조정
+	// let marginLeftValue = -index * windowWidth + 'px';
+	$('.slides').css('margin-left', '0px');
+});
+
+$(function () {
+	windowWidth = $(window).width();
+	setInterval(function () {
+		$('.slides > img:first-of-type').animate(
+			{
+				'margin-left': -windowWidth + 'px',
+			},
+			1000,
+			function () {
+				$('.slides > img:first-of-type').append('.slides');
+				$('.slides').css('margin-left', '0px');
+			}
+		);
+	}, 2000);
+});
+
+// $(document).ready(function () {
+// 	let currentIndex = 0;
+// 	const slideCount = $('.carousel .slides img').length;
+// 	const slideWidth = $('.carousel').width();
+// 	const $slides = $('.carousel .slides');
+
+// 	$slides.append($slides.children().first().clone());
+
+// 	$slides.css('width', (slideCount + 1) * slideWidth + 'px');
+
+// 	function goToSlide(index) {
+// 		$slides.css('margin-left', -index * slideWidth + 'px');
+// 	}
+
+// 	function nextSlide() {
+// 		currentIndex = (currentIndex + 1) % slideCount;
+// 		goToSlide(currentIndex);
+
+// 		if (currentIndex === slideCount) {
+// 			setTimeout(function () {
+// 				$slides.css('transition', 'none');
+// 				$slides.css('margin-left', '0px');
+// 				currentIndex = 0;
+// 			}, 1000); // Timeout duration should match the animation duration
+// 		}
+// 	}
+
+// 	function prevSlide() {
+// 		currentIndex = (currentIndex - 1 + slideCount) % slideCount;
+// 		goToSlide(currentIndex);
+// 	}
+
+// 	$('.control-next').click(function () {
+// 		nextSlide();
+// 	});
+
+// 	$('.control-prev').click(function () {
+// 		prevSlide();
+// 	});
+
+// 	// 자동 슬라이드
+// 	setInterval(function () {
+// 		nextSlide();
+// 	}, 3000);
+
+// 	// 윈도우 리사이즈시 슬라이드 위치 재조정
+// 	$(window).resize(function () {
+// 		$slides.children().css('width', $('.carousel').width() + 'px');
+// 		goToSlide(currentIndex);
+// 	});
+// });
